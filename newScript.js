aa1781e94bd3eb737213013973265f834cd4d93a
barlydjaja <barlydjaja@gmail.com>
2020-10-22 15:28:22 +0700

    menyelesaikan tugas

--- newScript.js
+++ newScript.js
@@ -1,8 +1,11 @@
 
-const getUser = (id) => {
+
+const getUser = () => {
+    id = document.querySelector("#exampleFormControlSelect1").value
+    id = Number(id)
     const response = new Promise((resolve, reject)=>{
         setTimeout(() => {
-            if (typeof(id) !== "number"){
+            if (!id){
                 console.log(Error("invalid ID"))
             } else if (id<10){
                 resolve ({
@@ -26,29 +29,51 @@ const getUser = (id) => {
 // menggunakan metode chain promise
 // const handleOnClick = () =>{
 //     console.log("clicked..")
-//     const foo = getUser(8)
+//     const foo = getUser()
 //     foo   
-//         .then (hasil => console.log(hasil))
-//         .catch (error => console.log(error))
+//         .then (hasil => {
+//             document.getElementById("hasilType").innerHTML = hasil.sucess
+//             document.getElementById("hasilID").innerHTML = hasil.id
+//             document.getElementById("hasilMessage").innerHTML = hasil.message
+//             console.log(hasil)
+//         })
+//         .catch (error => {
+//             document.getElementById("hasilType").innerHTML = error.sucess
+//             document.getElementById("hasilID").innerHTML = error.id
+//             document.getElementById("hasilMessage").innerHTML = error.message
+//         })
 // }
 
 
 
 
 // menggunakan async-await
-const handleOnClick = async() =>{
-        console.log("clicked..")
-        const hasil = await getUser(11).catch(error => error)
-        console.log(hasil)
-}
+// const handleOnClick = async() =>{
+//         console.log("clicked..")
+//         const hasil = await getUser().catch(err=>err)
+//         document.getElementById("hasilType").innerHTML = hasil.sucess
+//         document.getElementById("hasilID").innerHTML = hasil.id
+//         document.getElementById("hasilMessage").innerHTML = hasil.message
+//         console.log(hasil)
+// }
 
 
 
 // menggunakan Promise.all
 // const handleOnClick = () =>{
 //     console.log("clicked..")
-//     const hasil = Promise.all(getUser(8))
-//     hasil.then(console.log(hasil))
+//     Promise.all([getUser()])
+//     .then(hasil => {
+//         document.getElementById("hasilType").innerHTML = hasil.sucess
+//         document.getElementById("hasilID").innerHTML = hasil.id
+//         document.getElementById("hasilMessage").innerHTML = hasil.message
+//         console.log(hasil)
+//     })
+//     .catch(error=> {
+//         document.getElementById("hasilType").innerHTML = error.sucess
+//         document.getElementById("hasilID").innerHTML = error.id
+//         document.getElementById("hasilMessage").innerHTML = error.message
+//     })
 // }
 
-handleOnClick()
+// handleOnClick()
